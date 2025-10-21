f5bc973d40080ba2df5964d8a7a84d06d4f13701
= <=>
2025-10-21 18:01:27 +0200

    Initial Commit

---
+++ kcaliflow/kcaliflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  kcaliflowApp.swift
+//  kcaliflow
+//
+//  Created by René Jossen on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct kcaliflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ kcaliflowTests/kcaliflowTests.swift
@@ -0,0 +1,16 @@
+//
+//  kcaliflowTests.swift
+//  kcaliflowTests
+//
+//  Created by René Jossen on 21.10.2025.
+//
+
+import Testing
+
+struct kcaliflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
